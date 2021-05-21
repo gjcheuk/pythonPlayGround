49bbaefb2b440a183ea40526d40abff1dde859a1
jcheuk <cheukgeejoy@gmail.com>
2021-05-21 04:46:55 +0000

    commit producer and create consumer

---
+++ kafKaStream/kafkaProducer.py
@@ -0,0 +1,51 @@
+import psutil
+from json import dumps
+from confluent_kafka import Producer
+from datetime import datetime
+from datetime import timedelta
+import sys
+
+
+def fetch_cpu_load():
+    try:
+        return "Cpu Load is : " + str(psutil.cpu_percent(0.1)) + "%"
+    except Exception as e:
+        print("Error getting cpu load")
+        print(str(e))
+
+
+def serialize(item):
+    try:
+        return dumps(item, default=lambda x: x.__dict__)
+    except Exception as e:
+        print("Error serialising item")
+        print(str(e))
+
+
+def producer_setup():
+    try:
+        p = Producer(
+            {
+                'bootstrap.servers': 'localhost:9092'
+            })
+        return p
+    except Exception as e:
+        print("Error setting up Producer")
+        print(str(e))
+
+
+def delivery_callback(err, msg):
+    if err:
+        sys.stderr.write('%% Message failed delivery: %s\n' % err)
+    else:
+        sys.stderr.write('%% Message delivered to %s [%d] @ %d\n' %
+                (msg.topic(), msg.partition(), msg.offset()))
+
+
+start_time = datetime.now()
+current_time = datetime.now()
+while current_time - start_time < timedelta(seconds = 10):
+    kafka_prod = producer_setup()
+    kafka_prod.produce('ComputingForGeeksTopic', value=serialize(fetch_cpu_load()), callback=delivery_callback)
+    kafka_prod.poll()
+    current_time = datetime.now()
